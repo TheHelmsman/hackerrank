492bec31236f84245d00163e238357e83f69638b
TheHelmsman <TheHelmsman@users.noreply.github.com>
2019-02-26 18:19:01 +0300

    Create BetweenTwoSets.js

---
+++ BetweenTwoSets.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the getTotalX function below.
+ */
+function getTotalX(a, b) {
+    /*
+     * Write your code here.
+     */
+    let result = 0;
+    let mx = Math.max(...a);
+    let mn = Math.min(...b);
+    for (let i = mx; i <= mn; i++) {
+        //console.log('mx: '+mx+' mn: '+mn+' i: '+i);
+        if (a.every(e => i % e === 0) && b.every(e => e % i === 0)) result++;
+    }
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nm = readLine().split(' ');
+
+    const n = parseInt(nm[0], 10);
+
+    const m = parseInt(nm[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    let total = getTotalX(a, b);
+
+    ws.write(total + "\n");
+
+    ws.end();
+}
